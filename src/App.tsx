541f4a5d8b93700475ef178eaa0afc7ae7326b76
TavionK <tavion.k.britt99@gmail.com>
2026-03-06 12:55:50 -0500

    Add `AddTask` component to `App` and pass `setTask` for task management

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 // Component Imports
 import Header from "./components/Header";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTask] = useState([]);
@@ -9,6 +10,7 @@ function App() {
     <>
       <main className="max-w-lg mx-auto px-4 mt-10">
         <Header taskCount={tasks.length} />
+        <AddTask setTask={setTask} />
       </main>
     </>
   );
