bf6fff71dc8d1eadc9ae59cb78f2adb88275be32
TavionK <tavion.k.britt99@gmail.com>
2026-03-06 23:57:27 -0500

    Add GSAP animations to `Header` with `SplitText` effect for heading and icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,50 @@
 import { SquareCheck } from "lucide-react";
+import { type RefObject, useEffect, useRef } from "react";
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
 
 interface HeaderProps {
   completeTaskCount: number;
 }
 
 export default function Header({ completeTaskCount }: HeaderProps) {
+  const heading: RefObject<null> = useRef(null);
+  const icon: RefObject<null> = useRef(null);
+
+  useEffect((): void => {
+    const tl: gsap.core.Timeline = gsap.timeline();
+    const split = new SplitText(heading.current, { type: "chars" });
+    // gsap.set(".lucide-square-check", { rotation: -40 });
+
+    tl.from(split.chars, { opacity: 0, y: 20, stagger: 0.05, duration: 0.6 });
+    tl.from(".lucide-square-check", {
+      opacity: 0,
+      y: -100,
+      duration: 0.6,
+      ease: "bounce.out(1.5)",
+    });
+
+    // tl.to(".lucide-square-check", {
+    //   rotation: 320,
+    //   duration: 2,
+    //   repeat: -1,
+    //   yoyo: true,
+    //   delay: 1,
+    //   repeatDelay: 2,
+    //   ease: "power1.inOut",
+    // });
+  }, []);
+
   return (
     <>
-      <h1>
+      <h1 ref={heading}>
         Simple{" "}
-        <SquareCheck
-          className="text-purple-600 inline size-7"
-          strokeWidth={2.5}
-        />{" "}
+        <span ref={icon} className="inline">
+          <SquareCheck
+            className="text-purple-600 inline size-7 -translate-y-0.5"
+            strokeWidth={2.5}
+          />
+        </span>
         <br />
         TODO List
       </h1>
