0b2b8a990d21b35d5a0f964a9fc4fa39f5332b2a
TavionK <tavion.k.britt99@gmail.com>
2026-03-07 01:08:39 -0500

    Enable task completion toggle in `ListItem` and update styling.

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,6 +1,6 @@
 import type { Task } from "../utils/tasks.ts";
-import { Trash } from "lucide-react";
-import { deleteTask } from "../utils/tasks.ts";
+import { Trash2 } from "lucide-react";
+import { deleteTask, toggleTask } from "../utils/tasks.ts";
 import type { Dispatch, SetStateAction } from "react";
 
 interface ListItemProps {
@@ -14,20 +14,26 @@ export default function ListItem({ listItem, setTask }: ListItemProps) {
   }
 
   return (
-    <li className="bg-gray-700 rounded-md py-2 px-4 flex justify-between items-center">
+    <li className="bg-gray-800 rounded-md py-2 px-4 flex justify-between items-center">
       <div className="flex items-center">
-        {/*<input*/}
-        {/*  type="checkbox"*/}
-        {/*  className="mr-2"*/}
-        {/*  checked={listItem.isComplete}*/}
-        {/*></input>*/}
+        <input
+          onChange={() => {
+            setTask((prevTask: Task[]) => toggleTask(prevTask, listItem));
+          }}
+          type="checkbox"
+          className="mr-2 "
+          checked={listItem.isComplete}
+        ></input>
         {listItem.text}
       </div>
       <button
         className="group rounded-md cursor-pointer p-2"
         onClick={handleDelete}
       >
-        <Trash className="size-4 cursor-pointer group-focus-visible:text-red-500 group-hover:text-red-500 transition-colors duration-200 ease-in-out" />
+        <Trash2
+          strokeWidth={2}
+          className="size-4 text-gray-600 cursor-pointer group-focus-visible:text-red-500 group-hover:text-red-500 transition-colors duration-200 ease-in-out"
+        />
       </button>
     </li>
   );
