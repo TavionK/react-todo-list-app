df3a3a4cb67f6517077e36ff2d235de803669cd2
TavionK <tavion.k.britt99@gmail.com>
2026-03-07 01:08:28 -0500

    Add `toggleTask` utility to update task completion status

--- src/utils/tasks.ts
+++ src/utils/tasks.ts
@@ -11,3 +11,12 @@ export function addTask(tasksArr: Task[], newTask: Task): Task[] {
 export function deleteTask(tasksArr: Task[], taskItem: Task) {
   return tasksArr.filter((task: Task) => taskItem.id !== task.id);
 }
+
+export function toggleTask(tasksArr: Task[], taskItem: Task) {
+  return tasksArr.map((task: Task) => {
+    if (taskItem.id === task.id) {
+      return { ...task, isComplete: !task.isComplete };
+    }
+    return task;
+  });
+}
