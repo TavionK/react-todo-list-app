c2629ada0fb6ec5f7e7c342313f50e5cfe82fc6c
TavionK <tavion.k.britt99@gmail.com>
2026-03-11 15:28:19 -0400

    Add border to `ListItem` for improved visual differentiation

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -16,7 +16,7 @@ export default function ListItem({ listItem, setTask }: ListItemProps) {
   }
 
   return (
-    <li className="bg-gray-800 rounded-md py-2 px-4 flex justify-between items-center text-lg">
+    <li className="bg-gray-800 border border-gray-600 rounded-md py-2 px-4 flex justify-between items-center text-lg">
       <div className="flex items-center">
         <Checkbox.Root
           className="flex items-center justify-center size-5 rounded-md appearance-none  bg-gray-800 outline-none border border-gray-500 data-[state=checked]:bg-purple-700 data-[state=checked]:border-purple-700 cursor-pointer mr-4"
